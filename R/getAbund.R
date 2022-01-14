94a144b33ca7c1cd22b2d53ebd2af42af89a34aa
Gian77 <gian.benucci@gmail.com>
2022-01-14 17:20:40 -0500

    new funciton

---
+++ R/getAbund.R
@@ -0,0 +1,22 @@
+getAbund <- function(physeq, rank){
+  tax_table(physeq)[tax_table(physeq)==""]<- NA
+  tax_table(physeq)[tax_table(physeq)=="unidentified"]<- NA
+  tax_table(physeq)[is.na(tax_table(physeq))]<-"Unclassified"
+  tax_table(physeq) <-
+      tax_table(physeq)[, !(colnames(tax_table(physeq)) %in% c("OTU_ID"))]
+  physeq_phylum <-
+      tax_glom(physeq, rank)
+  otu_physeq <-
+      taxa_sums(physeq_phylum)/sum(taxa_sums(physeq_phylum))*100
+  tax_physeq <-
+      as(tax_table(physeq_phylum), "matrix")
+  tax_physeq <-
+      as.data.frame(tax_physeq)
+  #tax_physeq <- tax_physeq[c(2)]
+  tax_physeq$abundance <-
+      as.vector(otu_physeq)
+  tax_physeq <-
+      tax_physeq[order(tax_physeq$abundance, decreasing = TRUE),]
+  return(tax_physeq)
+}
+
